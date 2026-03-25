72a8616694a239c75a0e8329eb9fc3e37c51b956
sytux-boop <sattu.santitt@gmail.com>
2026-03-24 21:19:06 -0300

    V2.0

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const express = require('express')
 const path = require('path')
 const dotenv = require('dotenv')
 
+const views = path.join(__dirname, 'views/')
+const public = path.join(__dirname, 'public/')
+
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname, "public/statics")))
+app.use(express.static(public))
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', '/index.html'))
+    res.sendFile(path.format({ root: "/", dir: views, base: "index.html"}))
 })
 
 app.listen(PORT, () => console.log(`Server running! http://localhost:${PORT}`))
